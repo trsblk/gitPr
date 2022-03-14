7b65b17719516819c291f03d7ac9daea63c12dfd
Taras Bilyk <tarasbilyk@Air-Taras.local>
2022-03-14 15:28:20 +0100

    remove event listener and changing color

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const h1 = document.querySelector("h1");
 console.log(h1);
 
-h1.addEventListener("click", function () {
-  p.classList.toggle("disappear");
-});
+// h1.addEventListener("click", function () {
+//   p.classList.toggle("disappear");
+// });
 
 // Adding something
 
